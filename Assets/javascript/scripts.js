37892fe29860e6734d7dadbeeec55ad07295b880
Andreya <avalabek@yahoo.com>
2017-11-08 10:34:16 -0700

    js responding to key input and showing on page

--- Assets/javascript/scripts.js
+++ Assets/javascript/scripts.js
@@ -1,46 +1,72 @@
+  // Creates an array that lists out all of the cities 
+    var wordBank = ["Havana"];
 
-
-//change the script above to be src link //
-
-    //link to bootstrap if using//
-
-    // Creates an array that lists out all of the cities 
-    var wordBank = ["Havana","Beruit","Oslo","Istanbul","Nice","Granada", "Copenhagen","Oaxaca","Reykjavik","Zagreb","Zurich", "Bratislava", "Budapest"];
+    //"Beruit","Oslo","Istanbul","Nice","Granada", "Copenhagen","Oaxaca","Reykjavik","Zagreb","Zurich", "Bratislava", "Budapest"];
         
-    //make computer choose one city at random (tested in console -- works!)
-    var city = wordBank[Math.floor(Math.random() * wordBank.length)]; 
-
+    
     //array to hold --- equal to word length
     var answerArray = [];
     for (var i = 0; i < wordBank.length; i++){
       answerArray[i] = "_";
     };
- 
-
-    //take a guess from player
-
-    
-    // Do I need alphabet array??? ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o", "p","q","r", "s","t","u","v","w","x","y","z"];
-
-    /*now i am getting a little lost. Need to make a condition that if the user chooses a letter out of the array so index[0]charAt[0] and unitl it reaches computerChoices[0] i < computerChoices[0].length scrolling through the whole array
-    var userChoices = function(){
-
-    };*/
-
-    // Creating variables to hold the number of wins. They start at 0.
+ // Creating variables to hold the number of wins. They start at 0.
     var wins = 0;
-    
+    var losses = 0; 
+    //change losses to list letters guessed
 
-    // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
-    	console.log(event);
+ // This function is run whenever the user presses a key.
+
+//the conditionals are all within the event brackets
+document.onkeyup = function(event){
+
+//determines which key was pressed.
+	var userGuess = event.key;
+	
+//make computer choose one city at random (tested in console -- works!)
+    var city = wordBank[Math.floor(Math.random() * wordBank.length)]; 
+
+// This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+      //if ((userGuess === "h") || (userGuess === "a") || (userGuess === "v")) || (userGuess === "n") {
+
+        if ((userGuess === "h") && (city === "Havana")) {
+          wins++;
+        } else if ((userGuess === "a") && (city === "Havana")){
+          wins++;
+        } else if ((userGuess === "v") && (city === "Havana")) {
+          wins++;
+        } else if ((userGuess === "n") && (city === "Havana")) {
+          wins++;
+        } else { losses++}
+
+// Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+        var html =
+          "<p>You chose: " + userGuess + "</p>" +
+          "<p>Wins: " + wins + "</p>" +
+          "<p>losses: " + losses + "</p>";
+
+        // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#game").innerHTML = html;
+      }        
+//};
+
+
+
+//	var userText = document.getElementById("user-text");
+ //	document.onkeyup = function(event) {
+   //     userText.textContent = event.key;
+      
+    // document.onkeyup = function(event) {
+    //	console.log(event);
       // Determines which key was pressed.
-      var userGuess = event.key;
+     // var userGuess = event.key;
 
-      var remainingLetters = computerPick.length;
-        for (var i = 0; i > wordBank.length; i++){
+     
 
-        };
+
+//      var remainingLetters = city.length - userGuess;
+        //for (var i = 0; i > wordBank.length; i++){
+
+        //};
       
 //make variables to keep track of at top of page put all variables at top that you want to access fix remaining letters to be remaining guesses and letters guessed new array
 //the above bit is not right
@@ -48,10 +74,10 @@
 //.length is not lit up? ok? 
 
       // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-      if ((userGuess === computerPick.charAt(i)) && (remainingLetters !== 0))  { 
+ //     if ((userGuess === city.charAt(i)) && (remainingLetters !== 0))  { 
    
-          console.log (answerArray.join())
-    };
+   //       console.log (answerArray.join())
+   // };
 
     //how to tell it to stop when guesses complete?
 /* if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
@@ -72,15 +98,15 @@
           ties++;
         }
 */
-        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-        /*var html =
-          "<p>You chose: " + userGuess + "</p>" +
-          //the one below needs to show the ---- move code here
-          "<p>Current city: " + answerArray.join() + "</p>" +
-          "<p>wins: " + wins + "</p>" ;
+        //Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+        // var html =
+          // "<p>You chose: " + userGuess + "</p>" +
+          // the one below needs to show the ---- move code here
+          // "<p>Current city: " + answerArray.join() + "</p>" +
+          // "<p>wins: " + wins + "</p>" ;
 
         // Set the inner HTML contents of the #game div to our html string
-        document.querySelector("#game").innerHTML = html;
-      */
-    };
+        // document.querySelector("#game").innerHTML = html;
+      
+ //   };
   
