30cbdf5e096882f7a2e428184b6b9d4208ad6e8f
Andreya <avalabek@yahoo.com>
2017-11-09 16:07:11 -0700

    blanks fill in as correct letter guessed

--- Assets/javascript/scripts.js
+++ Assets/javascript/scripts.js
@@ -1,7 +1,6 @@
-  // Creates an array that lists out all of the cities 
-    var wordBank = ["Havana"];
-    //,"Beruit","Oslo","Istanbul","Nice","Granada", "Copenhagen","Oaxaca","Reykjavik","Zagreb","Zurich", "Bratislava", "Budapest"];
-    //note: I have taken everything out of word bank just to see if I can get it to work with one item only.
+   
+    var wordBank = ["Havana","Beruit","Oslo","Istanbul","Nice","Granada", "Copenhagen","Oaxaca","Reykjavik","Zagreb","Zurich", "Bratislava", "Budapest"];
+    
     var city = wordBank[Math.floor(Math.random() * wordBank.length)];       
     
     //array to hold blanks equal to city  length
@@ -9,7 +8,10 @@
     for (var i = 0; i < city.length; i++){
       answerArray[i] = "_";
     }
-    var remainingLetters = city.length
+    
+    console.log (city.substr(-10));
+
+    var remainingLetters = city.length;
  // Creating variables to hold the number of wins. They start at 0.right now have losses in but will delete once working
     var wins = 0;
     
@@ -17,6 +19,7 @@
 
     var blanks; 
     var count = 0;
+
          
    /*this isn't workign but idea is start up starts game again prints random blanks
    function startUp() {
@@ -38,7 +41,10 @@ document.onkeyup = function(event){
 
 //determines which key was pressed.
 	var userGuess = event.key;
-	var lettersPicked = userGuess;
+
+//isn't working as userGuess changes with each guess
+	var lettersPicked = [userGuess + " "];
+  lettersPicked.push(userGuess)
 	
 //make computer choose one city at random (tested in console -- works!)
     
@@ -55,39 +61,42 @@ document.onkeyup = function(event){
 
 
 
-// This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-      //if ((userGuess === "h") || (userGuess === "a") || (userGuess === "v")) || (userGuess === "n") {
 
 
-        if ((userGuess === "h") && (city === "Havana")) {
-          wins++;
-        } else if ((userGuess === "a") && (city === "Havana")){
-          wins++;
-        } else if ((userGuess === "v") && (city === "Havana")) {
-          wins++;
-        } else if ((userGuess === "n") && (city === "Havana")) {
-          wins++;
-        } else if (guessesLeft >=1 ){
-          guessesLeft--; 
-        }
-          else if (guessesLeft === 0) { 
+//the below works!!
+           if (guessesLeft === 0) { 
           document.querySelector("#cityName").innerHTML = "Stay in Arizona.";
           document.querySelector("#title").innerHTML = "Game over! Try again.";
-          
         }
-      
-        //OK now you need the game to start again. how to do this? Also, can you put all this info in its own function 
-        //that you can call for each time user loses 
-        
-        
-//Need to somehow get each character from the city word and evaluate it so would that be city.charAt[i]
+          else {
+            for (var j = 0; j < city.length; j++){
+              if (city[j] === userGuess) {
+                answerArray[j] = userGuess;
+                remainingLetters--;
+              }
+             }
+          }
+
+//Need to somehow get each character from the city word and evaluate it so 
+// would that be city.charAt[i]
 //also wins only works when only havana is in. need to write if clauses for all? that seems silly
+
+
+
+
+
+//OK now you need the game to start again. how to do this? 
+// Also, can you put all this info in its own function 
+//that you can call for each time user loses 
+        
+        
+
        
         
 // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
         var html =
           "<p>  " + answerArray.join(" ") + "</p>" +
-          "<p>You chose: " + userGuess + "</p>" +
+          "<p>You chose: " + lettersPicked + "</p>" +
           "<p>Wins: " + wins + "</p>" +
           "<p>Letters picked already: " + userGuess +  "</p>" +
           "<p>Guesses Left: " + guessesLeft + "</p>";
